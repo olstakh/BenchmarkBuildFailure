c0fbf10538fed94f7cd6353dde51334e4898e9f1
Alex Stakh <olstakh@microsoft.com>
2023-07-21 15:18:47 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// --------------------------------------------------------------
+
+using BenchmarkDotNet.Running;
+
+namespace Microsoft.FrontlineWorker.Service.Benchmark;
+
+public class Program
+{
+    public static void Main(string[] args) =>
+
+        // Make sure to run it in release mode
+        // Example: dotnet run --project Microsoft.FrontlineWorker.Service.Benchmark.csproj -c Release --filter * --disableLogFile --join --envVars ASPNETCORE_ENVIRONMENT:Development
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+}
---
+++ TestBenchmark.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// --------------------------------------------------------------
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+
+namespace Benchmark;
+
+[SimpleJob(RunStrategy.ColdStart, iterationCount: 1)]
+public class TestBenchmark
+{
+    [Benchmark(OperationsPerInvoke = 1)]
+    public void Test()
+    {
+        Thread.Sleep(10);
+    }
+}
